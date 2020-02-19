b070a1360cec4764ba83f3b3fda9a7aa85853134
Koen Verboven <koenverboven@gmail.com>
2020-02-19 08:42:41 +0100

    aanpassing Weer-controller , veldje warm weg gedaan

--- packt_webapp_1/Controllers/WeatherForecastController.cs
+++ packt_webapp_1/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace packt_webapp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
